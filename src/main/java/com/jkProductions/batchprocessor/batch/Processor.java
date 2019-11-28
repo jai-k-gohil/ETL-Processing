6fbabd7005ea7516f89cc34756051c640cbcc605
jai-k-gohil <jai.k.gohil@gmail.com>
2019-11-29 00:28:50 +0530

    adding processor for transformation

---
+++ src/main/java/com/jkProductions/batchprocessor/batch/Processor.java
@@ -0,0 +1,31 @@
+package com.jkProductions.batchprocessor.batch;
+
+import org.springframework.batch.item.ItemProcessor;
+import com.jkProductions.batchprocessor.model.User;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by
+ * @author jaikishorgohil
+ */
+
+@Component
+public class Processor implements ItemProcessor<User, User> {
+    private static final Map<String, String> DEPT_NAMES = new HashMap<>();
+
+    public Processor() {
+        DEPT_NAMES.put("001","Technology");
+        DEPT_NAMES.put("002","Accounts");
+        DEPT_NAMES.put("003","Operations");
+    }
+
+    @Override
+    public User process(User user) throws Exception{
+        //get DEPT code and transform it to respective name
+        user.setDept(DEPT_NAMES.get(user.getDept()));
+        return user;
+    }
+}
